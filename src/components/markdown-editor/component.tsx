e9e487c020f5e1d936242f6ba97b41325bc5652d
Chris Griffith <chris@octopuscreative.com>
2018-06-21 22:26:47 -0700

    Add linting and slate.js

--- src/components/markdown-editor/component.tsx
+++ src/components/markdown-editor/component.tsx
@@ -1,9 +1,56 @@
 import * as React from 'react';
+import { Value } from 'slate';
+import { Editor } from 'slate-react';
 
-class MarkdownEditorComponent extends React.Component<{}, {}> {
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A line of text in a paragraph.',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
+
+interface Props {}
+
+interface State {
+  value: any; // tslint:disable-line no-any
+}
+
+class MarkdownEditorComponent extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      value: initialValue,
+    };
+  }
 
   public render() {
-    return <div>MarkdownEditorComponent</div>;
+    return (
+      <Editor
+        value={this.state.value}
+        onChange={this.onChange}
+      />
+    );
+  }
+
+  // tslint:disable-next-line no-any
+  private onChange = ({ value }: any) => {
+    this.setState({ value });
   }
 }
 
